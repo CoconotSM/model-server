82b50a0d296450bb23002f22af44971e6c75a504
Jaehyeonee <92504386+Jaehyeonee@users.noreply.github.com>
2023-08-27 02:22:37 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import model.convert_model 
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, send_file
 from flask_cors import CORS
 import ssl
-import base64
+import cv2
 from io import BytesIO
-from PIL import Image
+import base64
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
@@ -20,19 +20,22 @@ def convert():
     data = request.json
     input_text = data.get('input')
     script_data = data.get('script')
+    model_data = data.get('model')
 
-    # 딥러닝 모델을 사용하여 변환 작업 수행
-    output = model.convert_model.final_convert(input_text, script_data)
 
-    # PIL image to base64
+    # TODO: 딥러닝 모델을 사용하여 변환 작업 수행
+    output = model.convert_model.final_convert(script_data, input_text, model_data)
+    
+
     im_file = BytesIO()
-    output.save(im_file, format="PNG")
+    output.save(im_file, format ="PNG")
     im_bytes = im_file.getvalue()
     im_b64 = base64.b64encode(im_bytes)
-    # output.save('slide1.png')
+    
+
    
-    # base64 to ascii
     return jsonify({'image_url': im_b64.decode('ascii')})
 
 if __name__ == '__main__':
     app.run(port= 8000, debug=True)
+    
