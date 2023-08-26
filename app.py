5f569777839c20f4b3f1875c7dcd4897b85bdea8
Jaehyeonee <92504386+Jaehyeonee@users.noreply.github.com>
2023-08-27 02:13:59 +0900

    fix: 드롭다운 인덱스

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 import model.convert_model 
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, send_file
 from flask_cors import CORS
 import ssl
+import cv2
+from io import BytesIO
+import base64
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
@@ -17,47 +20,22 @@ def convert():
     data = request.json
     input_text = data.get('input')
     script_data = data.get('script')
+    model_data = data.get('model')
+
 
     # TODO: 딥러닝 모델을 사용하여 변환 작업 수행
-    output = model.convert_model.final_convert(script_data)
+    output = model.convert_model.final_convert(script_data, input_text, model_data)
+    
 
-    # 변환 결과를 반환
-    output.save('slide1.png')
+    im_file = BytesIO()
+    output.save(im_file, format ="PNG")
+    im_bytes = im_file.getvalue()
+    im_b64 = base64.b64encode(im_bytes)
+    
+
    
-    print(script_data)
-    return jsonify({'input': input_text, 'script': script_data})
+    return jsonify({'image_url': im_b64.decode('ascii')})
 
 if __name__ == '__main__':
     app.run(port= 8000, debug=True)
-
-
-
-# from flask import Flask, request, jsonify
-
-# app = Flask(__name__)
-
-
-# @app.route('/api/converto', methods =['POST'])
-# def convert():
-#     # 리액트에서 받아온 목차, 스크립트 
-#     data = request.json
-#     input_data = data.get('input')
-#     script_data = data.get('script')
     
-#     # 변환 모델 코드 작성
-#     print('성공')
-
-#     return jsonify({'input': input_data, 'script': script_data})
-
-# if __name__ == '__main__':
-#     app.run()
-
-# # from flask import Flask
-# # app = Flask(__name__)
-
-# # @app.route('/api/convert')
-# # def hello_world():
-# #     return 'Hello World!'
-
-# # if __name__ == '__main__':
-# #     app.run(port=8000, debug= True) 
